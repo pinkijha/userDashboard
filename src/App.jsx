c64ff2493231c715a47f45798df8faafba169b70
Pinki Jha <pinkijha1310@gmail.com>
2025-01-18 17:45:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Body from "./Components/Body"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="text-blue-600">
+        <Body/>
+      </div>
+    </>
+  )
+}
+
+export default App
